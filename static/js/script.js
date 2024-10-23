04d4c6c66c341eab580f4f6ca1f594c6be6f9cbf
RogelioKG <rogeliokg0813@gmail.com>
2024-10-23 16:56:14 +0800

    UPDATE: first commit

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+VanillaTilt.init(document.querySelector(".card"), {
+    max: 10,
+    speed: 2000,
+    perspective: 500,
+    gyroscope: true,
+    gyroscopeMinAngleX: -45,
+    gyroscopeMaxAngleX: 45,
+    gyroscopeMinAngleY: -45,
+    gyroscopeMaxAngleY: 45,
+});
